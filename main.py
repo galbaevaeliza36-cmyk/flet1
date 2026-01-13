238091a5e7c13df899251f894a04f24ab1f5f830
galbaevaeliza36-cmyk <galbaevaeliza@gmail.com>
2026-01-13 13:15:25 +0600

    добавить файл main.py

--- main.py
+++ main.py
@@ -2,49 +2,49 @@ import flet as ft
 from datetime import datetime, timezone, timedelta
 
 
-
-
-
-def main (page:ft.Page):
-    text_hello=ft.Text(value="Hello world")
+def main(page: ft.Page):
+    text_hello = ft.Text(value="Hello world")
     page.theme_mode = ft.ThemeMode.DARK
     text_hello.value = "Hello"
-    greeting_history=[]
+
+    greeting_history = []
     history_text = ft.Text("История привествий:")
+
+    name_input = ft.TextField(label="Введите что нибудь")
 
     def text_name(e):
         print(name_input.value)
         tz = timezone(timedelta(hours=6))
         now = datetime.now(tz)
-      
 
-        text_hello.value=f"{now.strftime("%Y-%m-%d %H:%M:%S")} hello {name_input.value}"
-        name_input.value=None
+        if name_input.value:
+            text_hello.value = f"{now.strftime('%Y-%m-%d %H:%M:%S')} hello {name_input.value}"
+            greeting_history.append(name_input.value)
 
-        greeting_history.append(name)
-        print(greeting_history)
-        history_text.value = "История привествий :\n" +",\n".join(greeting_history)
-        ""
+        name_input.value = None
+        history_text.value = "История привествий :\n" + ",\n".join(greeting_history)
+        page.update()
+
     def change_theme(e):
-        if page.theme_mode==ft.ThemeMode.DARK:
-           page.theme_mode=ft.ThemeMode.LIGHT
-        else:
-            page.theme_mode=ft.ThemeMode.DARK
+        page.theme_mode = (
+            ft.ThemeMode.LIGHT
+            if page.theme_mode == ft.ThemeMode.DARK
+            else ft.ThemeMode.DARK
+        )
+        page.update()
 
-    def clear_history(_):
-        print(greeting_history)
-        greeting
+    def sort_history(e):
+        greeting_history.sort(key=str.lower)
+        history_text.value = "История привествий :\n" + ",\n".join(greeting_history)
+        page.update()
 
-    
-
-    # text_button=ft.TextButton("SEND")
-    elevated_button=ft.ElevatedButton("send", on_click=text_name)
-    # icon_button=ft.IconButton(icon=ft.Icons.SEND)
+    elevated_button = ft.ElevatedButton("send", on_click=text_name)
+    sort_button = ft.ElevatedButton("Сортировать по алфавиту", on_click=sort_history)
     thememode_button = ft.IconButton(icon=ft.Icons.BRIGHTNESS_7, on_click=change_theme)
-    name_input = ft.TextField(label="Введите что нибудь",on_submit=text_name)  #label-подсказка в поле ввода
 
+    name_input.on_submit = text_name
 
-    #добоаление в страницу
-    page.add(text_hello, elevated_button, name_input,thememode_button )
+    page.add(text_hello,name_input,elevated_button,sort_button,thememode_button,history_text)
+
 
 ft.app(target=main)
